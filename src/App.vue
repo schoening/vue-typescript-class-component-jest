9fd845c8bbb2a7507fb93a5e335246a244404b43
schoening <oliverschoning@gmail.com>
2017-10-27 23:09:08 +0200

    Updated index.ts, added App.vue to make it more useful.

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <!-- message is not a prop -->
+        <!-- so the output is gonna be "!" -->
+        <regular-component :myProp="prop"></regular-component>
+        <class-component :myProp="prop"></class-component>
+        <property-decorator :myProp="prop"></property-decorator>
+    </div>
+</template>
+
+<script lang="ts">
+import RegularComponent from "./components/RegularComponent.vue";
+import ClassComponent from "./components/ClassComponent.vue";
+import PropertyDecorator from "./components/PropertyDecorator.vue";
+
+export default {
+    data () { 
+        return {
+            message: "Hello world!",
+            prop: "!" 
+        };
+    },
+    components: {
+        RegularComponent,
+        ClassComponent,
+        PropertyDecorator
+    }
+}
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,8 @@
-import { mount } from "vue-test-utils";
-import RegularComponent from "./components/RegularComponent.vue";
-import ClassComponent from "./components/ClassComponent.vue";
+import Vue from "vue";
+import App from "./App.vue";
 
-const wrapper1: any = mount(RegularComponent, {
-    el: "#test-1",
-    data: {
-        message: "Hello world",
-    },
-    propsData: {
-        myProp: "!"
-    }
+const vm = new Vue({
+    el: "#app",
+    template: "<app/>",
+    components: { App } 
 });
-
-console.log( wrapper1.vm.myProp );
-
-const wrapper2: any = mount(ClassComponent, {
-    el: "#test-2",
-    data: {
-        message: "Hello world",
-    },
-    propsData: {
-        myProp: "!"
-    }
-});
-
-console.log( wrapper2.vm.myProp );
